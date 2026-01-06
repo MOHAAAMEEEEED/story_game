d3218147e6733a57a897b3169c2730601b220d1b
Mohamed Walid <mo.waleedddd@gmail.com>
2026-01-06 06:08:29 +0200

    update allow_origins

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from backend.core.config import settings
 
 app= FastAPI(
     title="Choose your own adventure Game API",
@@ -12,7 +13,7 @@ app= FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=settings.ALLOWED_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
